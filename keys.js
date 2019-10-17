3fb2d496ed7c02845825f45b1a717ee56bce351e
Mercedes Walsh <sadieswalsh@gmail.com>
2019-10-17 16:26:22 -0700

    spotifyThis now works

--- keys.js
+++ keys.js
@@ -2,6 +2,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env. c49f328615234ecfadefe098a93e4795,
-  secret: process.env.b6f8fdf37c744c429d8a747c6aec3963
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,12 +1,3 @@
-// add code required to read and set any environment variables with the dotenv package
-require('dotenv').config()
-
-// add code required to import the keys.js file and store it in a variable
-var keys = require('./keys.js')
-
-// should be able to access keys like:
-// var spotify = new Spotify(keys.spotify)
-
 // make it so liri.js can take in one of the following commands:
 // --------------------
 // `concert-this`
@@ -32,3 +23,154 @@ var keys = require('./keys.js')
 // `node liri.js do-what-it-says`
 // ^^^ this should:
 // ***using 'fs' node package, takes text in random.txt and uses it to call LIRI's commands {it should run `spotify-this-song` for 'i want it that way' in random.txt}
+
+//  0     1           2              3
+// node liri.js spotifyThis <song name>
+
+//  0     1         2           3
+// node lirl.js movieThis <movie name>
+
+//  0     1         2          3
+// node liri.js concertThis <artist>
+
+//  0     1          2
+// node liri.js doWhatItSays
+
+// add code required to read and set any environment variables with the dotenv package
+require('dotenv').config()
+
+// require constants
+const request = require('request')
+const fs = require('fs')
+const keys = require('./keys.js')
+const Spotify = require('node-spotify-api')
+const spotify = new Spotify(keys.spotify)
+
+// switch break
+const operator = process.argv[2]
+const input = process.argv[3]
+
+userInput(operator, input)
+
+function userInput (operator, input) {
+    switch (operator) {
+        case 'spotifyThis':
+            songInfo(input)
+            break
+        
+        case 'movieThis':
+            movieInfo(input)
+            break
+
+        case 'concertThis':
+            concertInfo(input)
+            break
+
+        case 'doWhatItSays':
+            backstreetBoys()
+            break
+
+        default:
+            console.log('please type spotifyThis, movieThis, concertThis, or doWhatItSays following node liri.js')
+    }
+
+}
+
+// spotify
+function songInfo(input) {
+    if (input === undefined) {
+        input = 'Tadow'
+    }
+    spotify.search(
+        {
+            type: 'track',
+            query: input
+        },
+        function (e, data) {
+            if (e) {
+                console.log(e)
+                return
+            }
+            const songs = data.tracks.items
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log('--------spotify song--------')
+                console.log('song name: ' + songs[i].name)
+                console.log('preview song: ' + songs[i].preview_url)
+                console.log('album: ' + songs[i].album.name)
+                console.log('artist: ' + songs[i].artists[0].name)
+                console.log('----------------------------')
+            }
+        }
+    )
+}
+
+// movie
+function movieInfo(input) {
+    if (input === undefined) {
+        input = 'Austin Powers: International Man of Mystery'
+        console.log('------------------')
+        console.log('watch Austin Powers. https://www.imdb.com/title/tt0118655/')
+    }
+    const queryUrl = 'http://www.omdbapi.com/?t=" + inputParameter + "&y=&plot=short&apikey=trilogy'
+    request(queryUrl, function (e, r, body) {
+        // successful request
+        if (!e && r.statusCode === 200) {
+            const movies = JSON.parse(body)
+            console.log('--------movie info--------')
+            console.log('title: ' + movies.Title)
+            console.log('release year: ' + movies.Year)
+            console.log('IMDB rating: ' + movies.imdbRating)
+            console.log('rotton tomatoes rating: ' + rottenTomatoesValue(movies))
+            console.log('production country: ' + movies.Country)
+            console.log('language: ' + movies.Language)
+            console.log('plot: ' + movies.Plot)
+            console.log('actors: ' + movies.Actors)
+            console.log('------------------')
+        } else {
+            console.log('please input correctly')
+        }
+    })
+}
+
+// concert
+function concertInfo(input) {
+    const queryUrl = `https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp`
+    request(queryUrl, function(e, r, body) {
+        // successful request
+        if (!e && r.statusCode === 200) {
+            for (var i = 0; i < concerts.length; i++) {
+                console.log('--------concert info--------')
+                console.log('venue: ' + concerts[u].venue.name+'\n')
+                console.log('location: ' + concerts[i].venue.city+'\n')
+                console.log('date: ' + concerts[i].datetime)
+                console.log('--------------------')
+            }
+        } else {
+            console.log('please input correctly')
+        }
+    })
+}
+
+// rotten tomatoes
+function rottenTomatoes (data) {
+    return data.Ratings.find(function (item) {
+        return item.Source === 'Rotten Tomatoes'
+    })
+}
+
+function rottenTomatoesValue (data) {
+    return rottenTomatoes(data).Value
+}
+
+// random.txt
+function backstreetBoys() {
+    fs.readFile('random.txt', 'utf8', function(e, data) {
+        if (e) {
+            return console.log(e)
+        }
+        const dataArray = data.split(',')
+        input(dataArray[0], dataArray[1])
+    })
+}
+
